b4caa48ae205e03f1a5b1edda6bf0f838248fa95
HesamAhrari <h.ahrari@azkivam.com>
2023-06-11 16:25:32 +0330

    fix server.js file

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 #!/usr/bin/env node
 var prerender = require('./lib');
 
-var server = prerender();
+// var server = prerender();
 
-server.use(prerender.sendPrerenderHeader());
-server.use(prerender.browserForceRestart());
-// server.use(prerender.blockResources());
-server.use(prerender.addMetaTags());
-server.use(prerender.removeScriptTags());
-server.use(prerender.httpHeaders());
-
+// server.use(prerender.sendPrerenderHeader());
+// server.use(prerender.browserForceRestart());
+// // server.use(prerender.blockResources());
+// server.use(prerender.addMetaTags());
+// server.use(prerender.removeScriptTags());
+// server.use(prerender.httpHeaders());
+var server = prerender({
+  chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars']
+});
 server.start();
